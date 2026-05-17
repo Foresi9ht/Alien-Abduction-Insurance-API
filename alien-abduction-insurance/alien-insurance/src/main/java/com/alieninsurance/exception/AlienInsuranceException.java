bcdceaee9551bd03016159c4d2b455e313d96199
Foresi9ht <157278731+Foresi9ht@users.noreply.github.com>
2026-05-18 00:43:59 +0500

    Delete alien-abduction-insurance/alien-insurance directory

--- alien-abduction-insurance/alien-insurance/src/main/java/com/alieninsurance/exception/AlienInsuranceException.java
+++
@@ -1,7 +0,0 @@
-package com.alieninsurance.exception;
-
-public class AlienInsuranceException extends RuntimeException {
-    public AlienInsuranceException(String message) {
-        super(message);
-    }
-}
